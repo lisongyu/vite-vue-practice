a40063bc019302c3e4a8c0a31aab92a1b8a09eba
lisongyu <lisongyu@bailian.ai>
2022-10-17 07:23:25 +0800

    提交基本配置项完成

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from "vite"
+import {resolve} from 'path';
+import { defineConfig,normalizePath} from "vite"
 import vue from "@vitejs/plugin-vue"
 
+const variablePath = normalizePath(resolve('./src/assets/sass/variable.scss'))
 // https://vitejs.dev/config/
 export default defineConfig({
+   //设置别名
+   resolve: {
+    alias: {
+      '@': resolve('src'),
+      '@assets':resolve('src/assets')
+    },
+    extensions: [".js", ".ts", ".jsx", ".tsx", ".json", ".vue"]
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // additionalData 的内容会在每个scss的开头自动注入
+        additionalData: `@import "${variablePath}";`
+      }
+    },
+
+  },
+
   plugins: [vue()]
 })
